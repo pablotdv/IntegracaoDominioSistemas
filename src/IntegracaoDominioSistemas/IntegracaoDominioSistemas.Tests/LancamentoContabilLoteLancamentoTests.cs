766235b168ca69d664a1529840637344da89ae9a
pablotdvsm <pablotdvsm@gmail.com>
2017-03-31 09:06:21 -0300

    Geração da linha do lancamento

---
+++ src/IntegracaoDominioSistemas/IntegracaoDominioSistemas.Tests/LancamentoContabilLoteLancamentoTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegracaoDominioSistemas.Tests
+{
+    /// <summary>
+    /// Summary description for LancamentoContabilLoteLancamentoTests
+    /// </summary>
+    [TestClass]
+    public class LancamentoContabilLoteLancamentoTests
+    {
+        [TestMethod]
+        public void LoteLancamento_ToString()
+        {
+            LancamentoContabilLoteLancamento lancamento = new LancamentoContabilLoteLancamento()
+            {
+                CodigoSequencial = 2,
+                ContaDebito = 1500,
+                ContaCredito = 1700,
+                ValorLancamento = 55.00M,
+                CodigoHistorico = 0,
+                Historico = "VLR REF",
+                CodigoFilial = 3                
+
+            };            
+
+            var experado = "030000002000150000017000000000000055000000000VLR REF                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         0000003                                                                                                    ";
+            var retornado = lancamento.ToString();
+
+            Assert.AreEqual(experado, retornado);
+            Assert.AreEqual(664, retornado.Length);
+        }
+    }
+}
--- src/IntegracaoDominioSistemas/IntegracaoDominioSistemas/LancamentoContabilLoteLancamento.cs
+++ src/IntegracaoDominioSistemas/IntegracaoDominioSistemas/LancamentoContabilLoteLancamento.cs
@@ -1,7 +1,59 @@
 ﻿namespace IntegracaoDominioSistemas
 {
-    public class LancamentoContabilLoteLancamento
+    public class LancamentoContabilLoteLancamento : ILinha
     {
+        [Posicao(1, 2)]
+        public string Identificador { get { return "03"; } }
 
+        [Posicao(3, 9)]
+        [Preencher(PreencherTipo.ZerosEsquerda)]
+        public int CodigoSequencial { get; set; }
+
+        [Posicao(10, 16)]
+        [Preencher(PreencherTipo.ZerosEsquerda)]
+        public int ContaDebito { get; set; }
+
+        [Posicao(17, 23)]
+        [Preencher(PreencherTipo.ZerosEsquerda)]
+        public int ContaCredito { get; set; }
+
+        [Posicao(24, 38)]
+        [Preencher(PreencherTipo.ZerosEsquerda)]
+        [Numerico(13, 2)]
+        public decimal ValorLancamento { get; set; }
+
+        [Posicao(39, 45)]
+        [Preencher(PreencherTipo.ZerosEsquerda)]
+        public int CodigoHistorico { get; set; }
+
+        [Posicao(46, 557)]
+        [Preencher(PreencherTipo.BrancosDireita)]
+        public string Historico { get; set; }
+
+        [Posicao(558, 564)]
+        [Preencher(PreencherTipo.ZerosEsquerda)]
+        public int CodigoFilial { get; set; }
+
+        [Posicao(565, 664)]
+        [Preencher(PreencherTipo.BrancosDireita)]
+        public string Brancos { get { return ""; } }
+
+        IGerarLinha _gerarLinha;
+
+        public LancamentoContabilLoteLancamento(IGerarLinha gerarLinha)
+        {
+            _gerarLinha = gerarLinha;
+        }
+
+        public LancamentoContabilLoteLancamento()
+            : this(new GerarLinha())
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return _gerarLinha.Gerar(this);
+        }
     }
 }
